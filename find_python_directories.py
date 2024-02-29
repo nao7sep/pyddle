a8eda6aae4c57ad357c0196709576bb83fc9811d
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-02-29 17:49:48 +0900

    Add find_python_directories.py

---
+++ find_python_directories.py
@@ -0,0 +1,46 @@
+﻿# Created: 2024-02-29
+# This script finds all Python-looking directories on all drives/volumes.
+
+from debugging import is_debugging
+
+import os
+
+def is_python_looking_directory(path):
+    expected_directory_names = ['include', 'lib'] # These 2 are always expected.
+    for name in expected_directory_names:
+        if os.path.isdir(os.path.join(path, name)) == False:
+            return False
+    if is_debugging():
+        print(f"Possible Python directory: {path}") # There can be variants of Python installations.
+    expected_file_names = ['python.exe', 'python3.exe', 'python', 'python3']
+    for name in expected_file_names:
+        if os.path.isfile(os.path.join(path, name)):
+            return True
+    return False
+
+def get_all_drive_or_volume_paths():
+    if os.name == 'nt':
+        for drive_letter in [chr(code) for code in range(ord('A'), ord('Z') + 1)]:
+            if os.path.isdir(f"{drive_letter}:\\"):
+                yield f"{drive_letter}:\\"
+    else:
+        for volume_name in os.listdir('/Volumes'):
+            yield os.path.join('/Volumes', volume_name) # As a string.
+
+scanned_directory_count = 0
+
+for drive_or_volume_path in get_all_drive_or_volume_paths():
+    for directory_path, subdirectory_names, _ in os.walk(drive_or_volume_path):
+        scanned_directory_count += 1
+        print(f"Scanned {scanned_directory_count} directories.\r", end = "")
+        for subdirectory_name in subdirectory_names:
+            subdirectory_path = os.path.join(directory_path, subdirectory_name)
+            if is_python_looking_directory(subdirectory_path):
+                print(f"Python-looking directory: {subdirectory_path}")
+
+# In an non-debugging environment, the scanned directory count will be overwritten by the "Press Enter key" message.
+# In a debugging environment, the console should get to a new prompt, inserting a line break after the last scanned directory count.
+
+from debugging import display_press_enter_key_to_continue_if_not_debugging
+
+display_press_enter_key_to_continue_if_not_debugging()
