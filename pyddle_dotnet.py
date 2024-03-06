2a922b7f7183aa75771a6d45ab5259c34eaf4aa7
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-03-06 13:39:41 +0900

    Extract version strings

---
+++ pyddle_dotnet.py
@@ -0,0 +1,51 @@
+﻿# Created: 2024-03-06
+# This script contains functions related to compilation and distribution of .NET projects.
+
+import pyddle_file_system as file_system
+import re
+import xml.etree.ElementTree
+
+# ------------------------------------------------------------------------------
+#     Version strings
+# ------------------------------------------------------------------------------
+
+def extract_version_string_from_csproj_file(path):
+    with file_system.open_file_and_detect_utf_encoding(path) as file:
+        tree = xml.etree.ElementTree.parse(file)
+        root = tree.getroot() # Project
+
+        # https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.Element.find
+        property_group = root.find("PropertyGroup")
+
+        if property_group is not None:
+            version = property_group.find("Version")
+
+            # The Version element, with no child elements, can be considered falsy.
+            # "if version:" doesnt seem to work.
+            # If it's not None, it should contain a string.
+            if version is not None:
+                return version.text
+
+def extract_version_string_from_app_manifest_file(path):
+    with file_system.open_file_and_detect_utf_encoding(path) as file:
+        tree = xml.etree.ElementTree.parse(file)
+        root = tree.getroot() # assembly
+
+        assembly_identity = root.find("{urn:schemas-microsoft-com:asm.v1}assemblyIdentity")
+
+        if assembly_identity is not None:
+            # https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.Element.get
+            return assembly_identity.get("version")
+
+assembly_info_file_s_version_string_pattern = r"\[assembly:\s*AssemblyVersion\s*\(\"(?P<version>\d+\.\d+(\.\d+){0,2})\"\)\]"
+
+def extract_version_string_from_assembly_info_file(path):
+    with file_system.open_file_and_detect_utf_encoding(path) as file:
+        for line in file:
+            if line.lstrip().startswith("//"):
+                continue
+
+            match = re.match(assembly_info_file_s_version_string_pattern, line)
+
+            if match:
+                return match.group("version")
