59d02addacc80803b035c72f4f16b9436407cd75
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-03-16 02:52:41 +0900

    Add string manipulation methods, untested

--- pyddle_string.py
+++ pyddle_string.py
@@ -14,6 +14,10 @@ leveledIndents = [
     "                                    " # Index: 9
 ] # Length: 10
 
+# ------------------------------------------------------------------------------
+#     Equality
+# ------------------------------------------------------------------------------
+
 # A sugar coating method to reduce writing "==" in code.
 # Sometimes, we need to check the case-sensitivity of string comparisons.
 
@@ -71,6 +75,10 @@ def contains_casefold(str_array, str):
 
     return False
 
+# ------------------------------------------------------------------------------
+#     Partial equality
+# ------------------------------------------------------------------------------
+
 def startswith(str, prefix):
     return str.startswith(prefix)
 
@@ -80,6 +88,30 @@ def startswith_ignore_case(str, prefix):
 def startswith_casefold(str, prefix):
     return str.casefold().startswith(prefix.casefold())
 
+def equals_at(str, index, substring):
+    substring_length = len(substring)
+
+    if index + substring_length > len(str):
+        return False
+
+    return str[index : index + substring_length] == substring
+
+def equals_at_ignore_case(str, index, substring):
+    substring_length = len(substring)
+
+    if index + substring_length > len(str):
+        return False
+
+    return str[index : index + substring_length].lower() == substring.lower()
+
+def equals_at_casefold(str, index, substring):
+    substring_length = len(substring)
+
+    if index + substring_length > len(str):
+        return False
+
+    return str[index : index + substring_length].casefold() == substring.casefold()
+
 def endswith(str, suffix):
     return str.endswith(suffix)
 
@@ -88,3 +120,73 @@ def endswith_ignore_case(str, suffix):
 
 def endswith_casefold(str, suffix):
     return str.casefold().endswith(suffix.casefold())
+
+# ------------------------------------------------------------------------------
+#     Searching
+# ------------------------------------------------------------------------------
+
+def index_of(str, substring):
+    return str.index(substring)
+
+def index_of_ignore_case(str, substring):
+    return str.lower().find(substring.lower())
+
+def index_of_casefold(str, substring):
+    return str.casefold().find(substring.casefold())
+
+def index_of_any(str, substrings):
+    for index in range(len(str)):
+        for substring in substrings:
+            if equals_at(str, index, substring):
+                return index
+
+    return -1
+
+def index_of_any_ignore_case(str, substrings):
+    for index in range(len(str)):
+        for substring in substrings:
+            if equals_at_ignore_case(str, index, substring):
+                return index
+
+    return -1
+
+def index_of_any_casefold(str, substrings):
+    for index in range(len(str)):
+        for substring in substrings:
+            if equals_at_casefold(str, index, substring):
+                return index
+
+    return -1
+
+def last_index_of(str, substring):
+    return str.rindex(substring)
+
+def last_index_of_ignore_case(str, substring):
+    return str.lower().rfind(substring.lower())
+
+def last_index_of_casefold(str, substring):
+    return str.casefold().rfind(substring.casefold())
+
+def last_index_of_any(str, substrings):
+    for index in range(len(str) - 1, -1, -1):
+        for substring in substrings:
+            if equals_at(str, index, substring):
+                return index
+
+    return -1
+
+def last_index_of_any_ignore_case(str, substrings):
+    for index in range(len(str) - 1, -1, -1):
+        for substring in substrings:
+            if equals_at_ignore_case(str, index, substring):
+                return index
+
+    return -1
+
+def last_index_of_any_casefold(str, substrings):
+    for index in range(len(str) - 1, -1, -1):
+        for substring in substrings:
+            if equals_at_casefold(str, index, substring):
+                return index
+
+    return -1
