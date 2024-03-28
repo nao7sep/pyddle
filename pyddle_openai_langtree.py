28d48bb96781c7b368e206f1a4f194316d8fbfb0
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-03-29 00:23:29 +0900

    Like 1/10 of langtree

--- pyddle_openai_langtree.py
+++ pyddle_openai_langtree.py
@@ -1,3 +1,13 @@
+﻿# Created:
+#
+
+from __future__ import annotations
+import enum
+import pyddle_datetime as dt
+import pyddle_openai as openai
+from typing import Union
+import uuid
+
 # ------------------------------------------------------------------------------
 #     Languages
 # ------------------------------------------------------------------------------
@@ -66,3 +76,101 @@ class OpenAiLanguage(enum.Enum):
     URDU = "urdu"
     VIETNAMESE = "vietnamese"
     WELSH = "welsh"
+
+# ------------------------------------------------------------------------------
+#     Nodes
+# ------------------------------------------------------------------------------
+
+class OpenAiBaseNode:
+    def __init__(self, role: openai.OpenAiRole, content):
+        # Set automatically:
+        self.guid = uuid.uuid4()
+        self.creation_utc = dt.get_utc_now()
+
+        # Relations:
+        # Foreign key.
+        # Necessary if the node is stored in a RDBMS.
+        self.parent_node_guid = None
+        # Wont be serialized; it'd make an infinite loop.
+        self._parent_node: OpenAiBaseNode = None
+        self.child_nodes: list[OpenAiBaseNode] = []
+
+        # Users must specify:
+        self.role: openai.OpenAiRole = role
+        self.content = content
+
+        # Optional info:
+        self.language: Union[OpenAiLanguage, str] = None
+
+        # Attributes:
+        # Initialized based on the data of the node.
+        self.summary = None
+        self.title = None
+
+    # I casually wrote about how I like to design CRUD operations.
+    # RI31 in FI56 Arguable Design Patterns.json
+
+    def create_child_node(self, child_node: OpenAiBaseNode):
+        ''' Returns the child node. '''
+
+        child_node.parent_node_guid = self.guid
+        child_node._parent_node = self
+        self.child_nodes.append(child_node)
+        return child_node
+
+    def update_child_node(self, child_node: OpenAiBaseNode):
+        ''' Returns None if the child node was not found. '''
+
+        for index, existing_node in enumerate(self.child_nodes):
+            if existing_node.guid == child_node.guid:
+                self.child_nodes[index] = child_node
+                return child_node
+
+        return None
+
+    def delete_child_node(self, child_node: OpenAiBaseNode):
+        ''' Returns None if the child node was not found. '''
+
+        for index, existing_node in enumerate(self.child_nodes):
+            if existing_node.guid == child_node.guid:
+                del self.child_nodes[index]
+                return child_node
+
+        return None
+
+class OpenAiNode(OpenAiBaseNode):
+    def __init__(self, role: openai.OpenAiRole, content):
+        super().__init__(role=role, content=content)
+        self.translations: dict[Union[OpenAiLanguage, str], OpenAiNodeTranslation] = {}
+
+    # And I wrote about these too: PG69 in FI56 Arguable Design Patterns.json
+
+    def create_translation(self, language: Union[OpenAiLanguage, str], translation: OpenAiNodeTranslation):
+        ''' Returns the translation. '''
+
+        translation.parent_node_guid = self.guid
+        translation._parent_node = self
+        self.translations[language] = translation
+        return translation
+
+    def update_translation(self, language: Union[OpenAiLanguage, str], translation: OpenAiNodeTranslation):
+        ''' Returns None if the translation was not found. '''
+
+        if language in self.translations:
+            self.translations[language] = translation
+            return translation
+
+        return None
+
+    def delete_translation(self, language: Union[OpenAiLanguage, str]):
+        ''' Returns None if the translation was not found. '''
+
+        if language in self.translations:
+            del self.translations[language]
+            return language
+
+        return None
+
+class OpenAiNodeTranslation(OpenAiBaseNode):
+    def __init__(self, role: openai.OpenAiRole, content):
+        super().__init__(role=role, content=content)
---
+++ test_openai_langtree.py
@@ -0,0 +1,8 @@
+﻿# Created:
+#
+
+import pyddle_debugging as debugging
+import pyddle_openai as openai
+import pyddle_openai_langtree as langtree
+
+debugging.display_press_enter_key_to_continue_if_not_debugging()
