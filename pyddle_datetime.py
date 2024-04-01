99f0408d269fe8ba9a918781833694d87cbf8c21
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-04-02 08:50:01 +0900

    Check

--- pyddle_datetime.py
+++ pyddle_datetime.py
@@ -11,17 +11,17 @@ def utc_to_roundtrip_string(utc):
 
     return utc.isoformat()
 
-def roundtrip_string_to_utc(str):
-    return datetime.datetime.fromisoformat(str)
+def roundtrip_string_to_utc(_str):
+    return datetime.datetime.fromisoformat(_str)
 
 # In .NET, I use code like ToString ("yyyyMMdd'T'HHmmss'-'fffffffK", CultureInfo.InvariantCulture) to generate an UTC timestamp that is able to roundtrip.
 # I get strings like: "20240307T074742-5919023Z".
 # With the following Python datetime format, we'll get strings like: "20240307T075556-212728Z", where the microseconds part is just one digit shorter.
 
 # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
-roundtrip_file_name_string_format = '%Y%m%dT%H%M%S-%fZ'
+ROUNDTRIP_FILE_NAME_STRING_FORMAT = '%Y%m%dT%H%M%S-%fZ'
 
 def utc_to_roundtrip_file_name_string(utc):
     """ Returns something like "20240307T075556-212728Z". """
 
-    return utc.strftime(roundtrip_file_name_string_format)
+    return utc.strftime(ROUNDTRIP_FILE_NAME_STRING_FORMAT)
--- pyddle_global.py
+++ pyddle_global.py
@@ -1,30 +1,32 @@
 ﻿# Created: 2024-03-08
-# This script contains things that must be initialized first and therefore must be imported first.
+# This script contains globally shared things and therefore must be imported first.
 
-# DONT import anything else.
+# Dont import anything else.
 import os
+# Dont import anything else.
 
 # ------------------------------------------------------------------------------
 #     Main script file path
 # ------------------------------------------------------------------------------
 
-main_script_file_path = None
+MAIN_SCRIPT_FILE_PATH = None
 
 def set_main_script_file_path(file_path):
     # "global" lets us modify the global variables outside of the function.
     # https://www.w3schools.com/python/python_variables_global.asp
 
-    global main_script_file_path
+    global MAIN_SCRIPT_FILE_PATH # pylint: disable=global-statement
+    # Expecting MAIN_SCRIPT_FILE_PATH to work like a field of a class.
 
     if not os.path.isfile(file_path):
         raise RuntimeError(f"File not found: {file_path}")
 
-    main_script_file_path = file_path
+    MAIN_SCRIPT_FILE_PATH = file_path
 
 def get_main_script_file_path():
     """ Raises a RuntimeError if the main script file path is not set. """
 
-    if not main_script_file_path:
+    if not MAIN_SCRIPT_FILE_PATH:
         raise RuntimeError("Main script file path not set.")
 
-    return main_script_file_path
+    return MAIN_SCRIPT_FILE_PATH
