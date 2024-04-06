8055fe1fc4b29d48b8b71ed1a5b40f97e67c7a9e
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-04-06 12:47:55 +0900

    Redesign data structure of langtree, unchecked

--- pyddle_openai_langtree.py
+++ pyddle_openai_langtree.py
@@ -1,4 +1,4 @@
-﻿# Created: 2024-04-04
+﻿# Created:
 # A module that helps us organize knowledge in a tree structure.
 
 import typing
@@ -10,51 +10,59 @@ import pyddle_datetime as pdatetime
 import pyddle_openai as popenai
 
 class LangTreeElement:
-    def __init__(self,
-                 guid=None,
-                 creation_utc=None):
+    def __init__(
+            self,
 
-        # These are required, but None can be specified to initialize them.
+            guid=None,
+            creation_utc=None):
+
         self.guid = guid if guid is not None else uuid.uuid4()
         self.creation_utc = creation_utc if creation_utc is not None else pdatetime.get_utc_now()
 
-        # Constructor parameters arent prepared for optional things.
-        self.parent_element_guid = None # May be used like a foreign key.
-        self.parent_element: LangTreeElement = None # Not serialized to avoid circular references.
+        self.parent_element_guid = None
+        self.parent_element: LangTreeElement = None
 
-        # Not serialized; they must be stored somewhere else, perhaps as global settings.
+        self.attributes: dict[str, LangTreeAttribute] = {}
+        self.translations: dict[typing.Union[popenai.OpenAiLanguage, str], LangTreeTranslation] = {}
+
         self.client: openai.OpenAI = None
         self.chat_settings: popenai.OpenAiChatSettings = None
         self.timeout = None
 
     def serialize_to_dict(self):
-        return {
-            "guid": str(self.guid),
-            "creation_utc": pdatetime.utc_to_roundtrip_string(self.creation_utc),
+        dictionary = {}
 
-            "parent_element_guid": self.parent_element_guid
-            # "parent_element" is not serialized.
-        }
+        dictionary["guid"] = str(self.guid)
+        dictionary["creation_utc"] = pdatetime.utc_to_roundtrip_string(self.creation_utc)
+
+        dictionary["parent_element_guid"] = str(self.parent_element_guid) if self.parent_element_guid is not None else None
+
+        if self.attributes:
+            dictionary["attributes"] = [attribute.serialize_to_dict() for attribute in self.attributes.values()]
+
+        if self.translations:
+            dictionary["translations"] = [translation.serialize_to_dict() for translation in self.translations.values()]
+
+        return dictionary
 
 class LangTreeMessage(LangTreeElement):
-    def __init__(self,
-                 user_role: popenai.OpenAiRole,
-                 content,
+    def __init__(
+            self,
 
-                 guid=None,
-                 creation_utc=None):
+            user_role: popenai.OpenAiRole,
+            content,
 
-        super().__init__(guid=guid, creation_utc=creation_utc)
+            guid=None,
+            creation_utc=None):
 
-        # "user_name" is optional and the rest are required.
-        # It would be more intuitive to have "user_name" in this order.
-        self.user_name = None # "name" alone could be confusing.
+        super().__init__(
+            guid=guid,
+            creation_utc=creation_utc)
+
+        self.user_name = None
         self.user_role: popenai.OpenAiRole = user_role
         self.content = content
 
-        # Generated usually in this order.
-        self.attributes: list[LangTreeAttribute] = []
-        self.translations: list[LangTreeTranslation] = []
         self.child_messages: list[LangTreeMessage] = []
 
     def serialize_to_dict(self):
@@ -63,19 +71,10 @@ class LangTreeMessage(LangTreeElement):
         dictionary.update(super().serialize_to_dict())
 
         if self.user_name:
-            dictionary["user_name"] = self.user_name # Optional.
+            dictionary["user_name"] = self.user_name
 
         dictionary["user_role"] = self.user_role.value
         dictionary["content"] = self.content
-
-        # Serialized only if they are truthy.
-        # We dont need a lot of lists where the values are [].
-
-        if self.attributes:
-            dictionary["attributes"] = [attribute.serialize_to_dict() for attribute in self.attributes]
-
-        if self.translations:
-            dictionary["translations"] = [translation.serialize_to_dict() for translation in self.translations]
 
         if self.child_messages:
             dictionary["child_messages"] = [child_message.serialize_to_dict() for child_message in self.child_messages]
@@ -83,14 +82,18 @@ class LangTreeMessage(LangTreeElement):
         return dictionary
 
 class LangTreeAttribute(LangTreeElement):
-    def __init__(self,
-                 name,
-                 value,
+    def __init__(
+            self,
 
-                 guid=None,
-                 creation_utc=None):
+            name,
+            value,
 
-        super().__init__(guid=guid, creation_utc=creation_utc)
+            guid=None,
+            creation_utc=None):
+
+        super().__init__(
+            guid=guid,
+            creation_utc=creation_utc)
 
         self.name = name
         self.value = value
@@ -101,19 +104,23 @@ class LangTreeAttribute(LangTreeElement):
         dictionary.update(super().serialize_to_dict())
 
         dictionary["name"] = self.name
-        dictionary["value"] = self.value # May be None.
+        dictionary["value"] = self.value
 
         return dictionary
 
 class LangTreeTranslation(LangTreeElement):
-    def __init__(self,
-                 language: typing.Union[popenai.OpenAiLanguage, str],
-                 content,
+    def __init__(
+            self,
 
-                 guid=None,
-                 creation_utc=None):
+            language: typing.Union[popenai.OpenAiLanguage, str],
+            content,
 
-        super().__init__(guid=guid, creation_utc=creation_utc)
+            guid=None,
+            creation_utc=None):
+
+        super().__init__(
+            guid=guid,
+            creation_utc=creation_utc)
 
         self.language: typing.Union[popenai.OpenAiLanguage, str] = language
         self.content = content
@@ -131,6 +138,6 @@ class LangTreeTranslation(LangTreeElement):
         dictionary.update(super().serialize_to_dict())
 
         dictionary["language"] = self.language_str
-        dictionary["content"] = self.content # Should never be None.
+        dictionary["content"] = self.content
 
         return dictionary
