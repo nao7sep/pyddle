16516ca0700f3bb8ab292d4dd3213d810daeeb50
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-04-21 10:43:25 +0900

    Rest of code_checker.py

--- code_checker.py
+++ code_checker.py
@@ -24,12 +24,12 @@ try:
     pyddle_directory_path = pkvs.read_from_merged_data(f"{KVS_KEY_PREFIX}pyddle_directory_path")
     pconsole.print(f"pyddle_directory_path: {pyddle_directory_path}")
 
-    project_directory_paths_str = pkvs.read_from_merged_data(f"{KVS_KEY_PREFIX}project_directory_paths")
-    project_directory_paths = [path_ for path_ in (path.strip() for path in project_directory_paths_str.split("|")) if path_]
+    project_directory_paths_str = pkvs.read_from_merged_data(f"{KVS_KEY_PREFIX}project_directory_paths") or ""
+    project_directory_paths = sorted([path_ for path_ in (path.strip() for path in project_directory_paths_str.split("|")) if path_])
     pconsole.print(f"project_directory_paths: {project_directory_paths}")
 
-    def check_code(project_directory_path, file_name_, prompt):
-        file_path = os.path.join(project_directory_path, file_name_)
+    def check_code(project_directory_path_, file_name_, prompt):
+        file_path = os.path.join(project_directory_path_, file_name_)
         code = pfs.read_all_text_from_file(file_path)
 
         messages: list[dict[str, str]] = []
@@ -42,32 +42,33 @@ try:
             messages = messages,
             stream = True)
 
-        chunk_strs: list[str] = []
-        reader = pstring.ChunkStrReader()
+        chunk_deltas: list[str] = []
+        reader = pstring.ChunkStrReader(indents = pstring.LEVELED_INDENTS[1])
+
+        pconsole.print(f"Response for {file_name_}:")
 
         for chunk in response:
             chunk_delta = popenai.extract_first_delta(chunk)
 
             if chunk_delta:
+                chunk_deltas.append(chunk_delta)
                 reader.add_chunk(chunk_delta)
 
                 chunk_str = reader.read_str()
-                chunk_strs.append(chunk_str)
                 pconsole.print(chunk_str, end = "")
                 sys.stdout.flush()
 
         chunk_str = reader.read_str(force = True)
-        chunk_strs.append(chunk_str)
         end = "" if chunk_str.endswith("\n") else "\n"
         pconsole.print(chunk_str, end = end)
 
-        response_str = "".join(chunk_strs).rstrip()
+        response_str = "".join(chunk_deltas).rstrip()
 
         utc_now = pdatetime.get_utc_now()
         date_string = utc_now.strftime("%Y%m%dT%H%M%SZ")
         file_name_without_extension, _ = os.path.splitext(file_name_)
         log_file_name = f"{date_string}-{file_name_without_extension}.log"
-        project_name = ppath.basename(project_directory_path)
+        project_name = ppath.basename(project_directory_path_)
         log_file_path = os.path.join(pyddle_directory_path, "code_checks", project_name, log_file_name)
 
         file_contents: list[str] = []
@@ -84,9 +85,84 @@ try:
         pfs.write_all_text_to_file(log_file_path, file_content_str)
         pconsole.print(f"Log file created: {log_file_path}")
 
+    def get_ignored_file_names(project_directory_path_):
+        project_name = ppath.basename(project_directory_path_)
+        ignored_file_path = os.path.join(pyddle_directory_path, "code_checks", project_name, "ignored.txt")
+
+        if os.path.isfile(ignored_file_path):
+            file_content_lines = pstring.splitlines(pfs.read_all_text_from_file(ignored_file_path))
+            return [path for path in file_content_lines if path and not path.startswith("#")]
+
+        return []
+
     CHECK_PROMPT = "Potential issues and improvements, as many and detailed as possible, please."
 
-except Exception: # pylint: disable=broad-except
+    if not pyddle_directory_path:
+        pconsole.print("pyddle_directory_path not set.", colors = pconsole.ERROR_COLORS)
+        sys.exit()
+
+    if not project_directory_paths:
+        pconsole.print("project_directory_paths not set.", colors = pconsole.ERROR_COLORS)
+        sys.exit()
+
+    pconsole.print("Projects:")
+    project_names = [ppath.basename(path) for path in project_directory_paths]
+    pconsole.print_numbered_options(project_names, indents = pstring.LEVELED_INDENTS[1])
+    project_index = pconsole.input_number("Select project or else to close: ") - 1
+
+    if project_index < 0 or project_index >= len(project_directory_paths):
+        sys.exit()
+
+    project_directory_path = project_directory_paths[project_index]
+    ignored_file_names = get_ignored_file_names(project_directory_path)
+    file_paths: list[str] = []
+
+    ignored_file_count = 0 # pylint: disable = invalid-name
+
+    for file_name in os.listdir(project_directory_path):
+        if pstring.endswith_ignore_case(file_name, ".py"):
+            if not pstring.contains_ignore_case(ignored_file_names, file_name):
+                file_paths.append(os.path.join(project_directory_path, file_name))
+
+            else:
+                ignored_file_count += 1
+
+    file_paths.sort()
+
+    pconsole.print(f"{len(file_paths) + ignored_file_count} Python files found.")
+    pconsole.print(f"{ignored_file_count} files ignored.")
+
+    if not file_paths:
+        sys.exit()
+
+    while True:
+        pconsole.print("Files:")
+        file_names = [ppath.basename(path) for path in file_paths]
+        pconsole.print_numbered_options(file_names, indents = pstring.LEVELED_INDENTS[1])
+        file_index_str = input("Select file or \"all\" or else to close: ")
+
+        try:
+            file_index = int(file_index_str)
+
+            if file_index < 0 or file_index >= len(file_paths):
+                break
+
+            check_code(project_directory_path, file_names[file_index], CHECK_PROMPT)
+
+            continue
+
+        except Exception: # pylint: disable = broad-except
+            pass
+
+        if pstring.equals_ignore_case(file_index_str, "all"):
+            for file_name in file_names:
+                check_code(project_directory_path, file_name, CHECK_PROMPT)
+
+            continue
+
+        break
+
+except Exception: # pylint: disable = broad-except
     pconsole.print(traceback.format_exc(), colors = pconsole.ERROR_COLORS)
 
 finally:
