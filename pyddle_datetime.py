1750e4e2a1f6ccf1c7706305195e3327e940c740
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-04-22 13:56:38 +0900

    Update ROUNDTRIP_FILE_NAME_STRING_FORMAT

--- pyddle_datetime.py
+++ pyddle_datetime.py
@@ -18,10 +18,15 @@ def roundtrip_string_to_utc(str_):
 # I get strings like: "20240307T074742-5919023Z".
 # With the following Python datetime format, we'll get strings like: "20240307T075556-212728Z", where the microseconds part is just one digit shorter.
 
+# Added: 2024-04-22
+# I changed the location of the UTC timezone indicator to preserve the consistency with the ISO 8601 format.
+# Even with a millisecond-level part attached, the core part should still comply with the format.
+# And, I also removed the then-redundant hyphen that separated the core part and the millisecond-level part.
+
 # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
-ROUNDTRIP_FILE_NAME_STRING_FORMAT = '%Y%m%dT%H%M%S-%fZ'
+ROUNDTRIP_FILE_NAME_STRING_FORMAT = '%Y%m%dT%H%M%SZ%f'
 
 def utc_to_roundtrip_file_name_string(utc):
-    """ Returns something like "20240307T075556-212728Z". """
+    """ Returns something like "20240307T075556Z212728". """
 
     return utc.strftime(ROUNDTRIP_FILE_NAME_STRING_FORMAT)
